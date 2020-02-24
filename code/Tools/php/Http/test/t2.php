539e46db1ef0bf1dd7aa97acc00d6042e115a80d
huahao <295506287@qq.com>
2020-02-24 09:55:54 +0800

    update

--- code/Tools/php/Http/test/t2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-
-if ($requestMethod == 'GET') {
-    echo 'helloworld';
-} elseif ($requestMethod == 'POST') {
-    echo $_POST['name'].', 你好！';
-}
